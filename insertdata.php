8138685dfad05f13b302355a6f28e894cee859e7
Endymion_Moses <mosessilem3@gmail.com>
2026-02-23 09:17:15 +0300

    Removed duplicate code from the insertdata.php file

--- insertdata.php
+++ insertdata.php
@@ -25,21 +25,4 @@ $connection->close();
 
 echo "data inserted successfully";
 
-
-foreach($sampleData as $item) {
-    $stmt->bind_param(
-        "ssssss",
-        $item["title"],
-        $item["description"],
-        $item["page_name"],
-        $item["page_fav_icon_path"],
-        $item['page_url'],
-        $item['created_at']
-    );
-    $stmt->execute();
-}
-$stmt->close();
-$connection->close();
-
-echo "data inserted successfully";
 ?>
